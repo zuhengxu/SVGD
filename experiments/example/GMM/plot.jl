2eb61128318a36eb0de634ac2977f0fc3ef674a8
zuhengxu <zuhengdavid@gmail.com>
2021-04-22 17:22:31 -0700

    tunning GMM

--- experiments/example/GMM/plot.jl
+++ experiments/example/GMM/plot.jl
@@ -5,7 +5,7 @@ include("example/common/plotting.jl")
 
 
 
-result_path = "example/GMM/result/"
+result_dir = "example/GMM/result/"
 fig_path = "example/GMM/figure/"
 
 #load KSD and ELBO
@@ -15,12 +15,16 @@ ksd_seq = [ K["sv"] K["sv_local"] K["asv"] K["asv_local"] ]
 elbo_seq = [ E["sv"] E["sv_local"] E["asv"] E["asv_local"] ]
 
 # making line plot
-X = 0:10:3000
-X1 = 500:10:3000
+X = 0:10:4000
+X1 = 500:10:4000
 label = ["sv" "sv_local" "asv" "asv_local"]
 line_plot(X,log1p.(ksd_seq .- 1.) ; folder = fig_path, name = "GMM_ksd.png", label = label, 
-            xlabel = "Iteration", ylabel = "KSD", legendfontsize=18)
+            xlabel = "Iteration", ylabel = "log KSD ", legendfontsize=18)
 
-line_plot(X1, -log1p.(-elbo_seq[51:end,:] .- 1.);  folder = fig_path, name = "GMM_elbo.png",ylim = (-6.5,-6.1),
-            xlabel = "Iteration", ylabel = "ELBO (log scale)", legendfontsize=18)
+line_plot(X, elbo_seq;  folder = fig_path, name = "GMM_elbo.png",label = label,
+            xlabel = "Iteration", ylabel = "ELBO (log scale)", legendfontsize=18, legend = :bottomright)
+
+
+line_plot(X1, log1p.(ksd_seq[51:end,:] .- 1.);  folder = fig_path, name = "GMM_ksd_zoom.png",
+            xlabel = "Iteration", ylabel = "log KSD ", legendfontsize=18)
 
--- experiments/example/logistic_reg/plot.jl
+++ experiments/example/logistic_reg/plot.jl
@@ -5,7 +5,7 @@ include("example/common/plotting.jl")
 
 
 
-result_path = "example/logistic_reg/result/"
+result_dir = "example/logistic_reg/result/"
 fig_path = "example/logistic_reg/figure/"
 
 #load KSD and ELBO
@@ -19,7 +19,7 @@ X = 0:10:2000
 X1 = 500:10:2000
 label = ["sv" "sv_local" ]
 line_plot(X,log1p.(ksd_seq .- 1.) ; folder = fig_path, name = "logis_ksd.png", label = label, 
-            xlabel = "Iteration", ylabel = "KSD", legendfontsize=18)
+            xlabel = "Iteration", ylabel = "log KSD", legendfontsize=18)
 
 line_plot(X, -log1p.(-elbo_seq .- 1.);  folder = fig_path, name = "logis_elbo.png",label = label, 
             xlabel = "Iteration", ylabel = "ELBO (log scale)", legendfontsize=18, legend = :bottomright)
